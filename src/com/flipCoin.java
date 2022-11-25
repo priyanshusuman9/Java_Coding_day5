4881ff1b436fe4b77578ac8b6324e7a9849145fc
priyanshusuman9 <priyanshusuman947@gmail.com>
2022-11-26 01:19:25 +0530

    commit

---
+++ src/com/flipCoin.java
@@ -0,0 +1,11 @@
+package com;
+
+public class flipCoin {
+        public static void main(String[] args) {
+            if (Math.random() < 0.5){
+                System.out.println("Heads");
+            }else{
+                System.out.println("Tails");
+            }
+        }
+    }
